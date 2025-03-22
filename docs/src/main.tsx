1cf10561afd19b3862870e19d87f681687ec7c68
fall-zhang <zhangbofei@163.com>
2025-03-23 00:49:06 +0800

    feat(typescript): 🚀 更新 docs 的配置，引入 saladict 文件

--- docs/src/main.tsx
+++ docs/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './views/index.css'
 import App from './views/App.tsx'
-
+import '@P/saladict-core/src/content/index.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+// import * as path  from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      // '@': path.resolve(__dirname, './src'),
+      // '@P': path.resolve(__dirname, '../packages'),
+    }
+  },
   plugins: [
     tailwindcss(),
     react(),
