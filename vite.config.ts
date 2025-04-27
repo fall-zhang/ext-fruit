134fcc368cd1f05d0636c9aaf304111a8b7fd25f
fall-zhang <zhangbofei@163.com>
2025-04-27 18:10:31 +0800

    tool(global): 🔧 更新依赖以及工具 vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,19 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import { fileURLToPath } from 'node:url'
+import react from '@vitejs/plugin-react'
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
-      '@': resolve(__dirname, './packages')
+      '@': resolve(__dirname, './packages/saladict-core/src'),
+      '@P': resolve(__dirname, './packages')
     }
   },
   plugins: [
+    react()
   ],
   test: {
     setupFiles: './tests/unit/setup.js',
