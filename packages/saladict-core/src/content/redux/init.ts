44197a9515c1f7306480405de0284313c1336a44
fall-zhang <zhangbofei@163.com>
2025-09-20 19:42:59 +0800

    refactor(typescript): ♻️ 重构浏览器扩展相关模块

--- packages/saladict-core/src/content/redux/init.ts
+++ packages/saladict-core/src/content/redux/init.ts
@@ -15,6 +15,7 @@ import { timer } from '@/_helpers/promise-more'
 import { MessageResponse } from '@/types/message'
 import { StoreDispatch, StoreState } from './modules'
 import { isTagName } from '@/_helpers/dom'
+import { isInNotebook } from '../../core/database/read'
 
 export const init = (dispatch: StoreDispatch, getState: () => StoreState) => {
   window.addEventListener('resize', () => {
@@ -48,33 +49,33 @@ export const init = (dispatch: StoreDispatch, getState: () => StoreState) => {
 
   message.addListener(msg => {
     switch (msg.type) {
-    case 'TEMP_DISABLED_STATE':
-      if (msg.payload.op === 'set') {
-        dispatch({ type: 'TEMP_DISABLED_STATE', payload: msg.payload.value })
-        setTimeout(() => {
-          const state = getState()
-          message.send({
-            type: 'SEND_TAB_BADGE_INFO',
-            payload: {
-              active: state.config.active,
-              tempDisable: state.isTempDisabled,
-              unsupported: isStandalonePage()
-                ? false
-                : !isTagName(document.body, 'body')
-            }
-          })
-        }, 0)
-        return Promise.resolve(true)
-      }
-      return Promise.resolve(getState().isTempDisabled)
+      case 'TEMP_DISABLED_STATE':
+        if (msg.payload.op === 'set') {
+          dispatch({ type: 'TEMP_DISABLED_STATE', payload: msg.payload.value })
+          setTimeout(() => {
+            const state = getState()
+            message.send({
+              type: 'SEND_TAB_BADGE_INFO',
+              payload: {
+                active: state.config.active,
+                tempDisable: state.isTempDisabled,
+                unsupported: isStandalonePage()
+                  ? false
+                  : !isTagName(document.body, 'body')
+              }
+            })
+          }, 0)
+          return Promise.resolve(true)
+        }
+        return Promise.resolve(getState().isTempDisabled)
 
 
-    case 'SEARCH_TEXT_BOX': {
-      const { searchHistory, historyIndex, text } = getState()
-      dispatch({
-        type: 'SEARCH_START',
-        payload: {
-          word:
+      case 'SEARCH_TEXT_BOX': {
+        const { searchHistory, historyIndex, text } = getState()
+        dispatch({
+          type: 'SEARCH_START',
+          payload: {
+            word:
               searchHistory[historyIndex]?.text === text
                 ? searchHistory[historyIndex]
                 : newWord({
@@ -82,83 +83,82 @@ export const init = (dispatch: StoreDispatch, getState: () => StoreState) => {
                   title: 'Saladict',
                   favicon: 'https://saladict.crimx.com/favicon.ico'
                 })
-        }
-      })
-      return isPopupPage() ? Promise.resolve(true) : Promise.resolve()
-    }
-
-    case 'WORD_SAVED': {
-      const { text } = getState()
-      if (text) {
-        message
-          .send<'IS_IN_NOTEBOOK'>({
-            type: 'IS_IN_NOTEBOOK',
-            payload: newWord({ text })
-          })
-          .then(isInNotebook => {
-            dispatch({ type: 'WORD_IN_NOTEBOOK', payload: isInNotebook })
-          })
+          }
+        })
+        return isPopupPage() ? Promise.resolve(true) : Promise.resolve()
       }
-      break
-    }
 
-    case 'ADD_NOTEBOOK': {
-      if (msg.payload.popup === isPopupPage()) {
-        dispatch({ type: 'ADD_TO_NOTEBOOK' })
+      case 'WORD_SAVED': {
+        const { text } = getState()
+        if (text) {
+          isInNotebook(newWord({ text }))
+            .then(isInNotebook => {
+              dispatch({ type: 'WORD_IN_NOTEBOOK', payload: isInNotebook })
+            })
+            .catch(err => {
+              console.warn('1011 ~ err:', err)
+            })
+        }
+        break
+      }
+
+      case 'ADD_NOTEBOOK': {
+        if (msg.payload.popup === isPopupPage()) {
+          dispatch({ type: 'ADD_TO_NOTEBOOK' })
+          return Promise.resolve(true)
+        }
+        return
+      }
+
+      case 'SWITCH_HISTORY': {
+        dispatch({ type: 'SWITCH_HISTORY', payload: msg.payload })
         return Promise.resolve(true)
       }
-      return
-    }
 
-    case 'SWITCH_HISTORY': {
-      dispatch({ type: 'SWITCH_HISTORY', payload: msg.payload })
-      return Promise.resolve(true)
-    }
-
-    case 'QS_PANEL_SEARCH_TEXT':
-      if (isQuickSearchPage()) {
+      case 'QS_PANEL_SEARCH_TEXT':
+        if (isQuickSearchPage()) {
         // request searching text, from other tabs
-        dispatch({ type: 'SEARCH_START', payload: { word: msg.payload } })
+          dispatch({ type: 'SEARCH_START', payload: { word: msg.payload } })
 
-        if (getState().isQSFocus) {
+          if (getState().isQSFocus) {
           // focus standalone panel
-          message.send({ type: 'OPEN_QS_PANEL' })
+            message.send({ type: 'OPEN_QS_PANEL' })
+          }
         }
-      }
-      return Promise.resolve()
+        return Promise.resolve()
 
-    case 'QS_PANEL_CHANGED':
-      if (!isQuickSearchPage()) {
-        dispatch({ type: 'QS_PANEL_CHANGED', payload: msg.payload })
-      }
-      return Promise.resolve()
+      case 'QS_PANEL_CHANGED':
+        if (!isQuickSearchPage()) {
+          dispatch({ type: 'QS_PANEL_CHANGED', payload: msg.payload })
+        }
+        return Promise.resolve()
 
-    case 'QS_PANEL_FOCUSED':
-      if (isQuickSearchPage()) {
-        const input = document.querySelector<
+      case 'QS_PANEL_FOCUSED':
+        if (isQuickSearchPage()) {
+          const input = document.querySelector<
             HTMLTextAreaElement | HTMLInputElement
           >(
             getState().isExpandMtaBox
               ? '.mtaBox-TextArea'
               : '.menuBar-SearchBox'
           )
-        if (input) {
-          input.focus()
-          input.select()
+          if (input) {
+            input.focus()
+            input.select()
+          }
         }
-      }
-      return Promise.resolve()
+        return Promise.resolve()
 
-    case 'GET_TAB_BADGE_INFO': {
-      const state = getState()
-      return Promise.resolve<MessageResponse<'GET_TAB_BADGE_INFO'>>({
-        active: state.config.active,
-        tempDisable: state.isTempDisabled,
-        unsupported: isStandalonePage()
-          ? false
-          : !isTagName(document.body, 'body')
-      })
-    }
+      case 'GET_TAB_BADGE_INFO': {
+        const state = getState()
+        return Promise.resolve<MessageResponse<'GET_TAB_BADGE_INFO'>>({
+          active: state.config.active,
+          tempDisable: state.isTempDisabled,
+          unsupported: isStandalonePage()
+            ? false
+            : !isTagName(document.body, 'body')
+        })
+      }
     }
   })
 
@@ -170,54 +170,54 @@ export const init = (dispatch: StoreDispatch, getState: () => StoreState) => {
 
   message.self.addListener(msg => {
     switch (msg.type) {
-    case 'SELECTION':
-      if (msg.payload.instant) {
-        lastInstantDate = Date.now()
-      } else if (Date.now() - lastInstantDate < 500) {
+      case 'SELECTION':
+        if (msg.payload.instant) {
+          lastInstantDate = Date.now()
+        } else if (Date.now() - lastInstantDate < 500) {
+          return Promise.resolve()
+        }
+        dispatch({ type: 'NEW_SELECTION', payload: msg.payload })
         return Promise.resolve()
-      }
-      dispatch({ type: 'NEW_SELECTION', payload: msg.payload })
-      return Promise.resolve()
 
-    case 'ESCAPE_KEY':
-      dispatch({ type: 'CLOSE_PANEL' })
-      return Promise.resolve()
+      case 'ESCAPE_KEY':
+        dispatch({ type: 'CLOSE_PANEL' })
+        return Promise.resolve()
 
-    case 'SEARCH_TEXT':
-      dispatch({ type: 'SEARCH_START', payload: { word: msg.payload } })
-      return Promise.resolve()
+      case 'SEARCH_TEXT':
+        dispatch({ type: 'SEARCH_START', payload: { word: msg.payload } })
+        return Promise.resolve()
 
-    case 'CLOSE_PANEL':
-      dispatch({ type: 'CLOSE_PANEL' })
-      return Promise.resolve()
+      case 'CLOSE_PANEL':
+        dispatch({ type: 'CLOSE_PANEL' })
+        return Promise.resolve()
 
-    case 'TRIPLE_CTRL':
-      if (!isPopupPage() && !isOptionsPage()) {
-        const state = getState()
-        if (state.config.tripleCtrl) {
-          if (state.config.qsStandalone) {
+      case 'TRIPLE_CTRL':
+        if (!isPopupPage() && !isOptionsPage()) {
+          const state = getState()
+          if (state.config.tripleCtrl) {
+            if (state.config.qsStandalone) {
             // focus if the standalone panel is already opened
-            message.send({ type: 'OPEN_QS_PANEL' })
-          } else if (!state.isShowDictPanel) {
-            dispatch({ type: 'OPEN_QS_PANEL' })
-            initTripleCtrl(dispatch, state)
+              message.send({ type: 'OPEN_QS_PANEL' })
+            } else if (!state.isShowDictPanel) {
+              dispatch({ type: 'OPEN_QS_PANEL' })
+              initTripleCtrl(dispatch, state)
+            }
           }
         }
-      }
-      return Promise.resolve()
+        return Promise.resolve()
 
-    case 'UPDATE_WORD_EDITOR_WORD':
-      dispatch({ type: 'WORD_EDITOR_STATUS', payload: msg.payload })
-      return timer(100).then(() => {
+      case 'UPDATE_WORD_EDITOR_WORD':
+        dispatch({ type: 'WORD_EDITOR_STATUS', payload: msg.payload })
+        return timer(100).then(() => {
         // wait till snapshot is taken
-        dispatch({
-          type: 'SEARCH_START',
-          payload: { word: msg.payload.word }
+          dispatch({
+            type: 'SEARCH_START',
+            payload: { word: msg.payload.word }
+          })
         })
-      })
 
-    case 'LAST_PLAY_AUDIO':
-      return Promise.resolve(getState().lastPlayAudio)
+      case 'LAST_PLAY_AUDIO':
+        return Promise.resolve(getState().lastPlayAudio)
     }
   })
 
