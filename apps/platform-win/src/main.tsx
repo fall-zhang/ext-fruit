f08d8b075458cf7a242574072ad724f7d1262c4e
fall-zhang <zhangbofei@163.com>
2026-04-15 17:32:44 +0800

    feat: 优化打开应用时的体验，减少白屏时间

--- apps/platform-win/src/main.tsx
+++ apps/platform-win/src/main.tsx
@@ -1,16 +1,27 @@
 import './assets/styles.css'
-import React from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+import { WebviewWindow } from '@tauri-apps/api/webviewWindow'
 
 const router = createRouter({ routeTree })
 const container = document.getElementById('root')!
 const root = createRoot(container)
 
-async function render () {
-  root.render(<React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>)
-}
-render()
+console.time('start render')
+// async function render () {
+root.render(<StrictMode>
+  <RouterProvider router={router} />
+</StrictMode>)
+// }
+// render()
+
+document.addEventListener('DOMContentLoaded', async () => {
+  // 也可以在 Vue 的 onMounted 或 React 的 useEffect 中调用
+  const searchWindow = await WebviewWindow.getByLabel('search-view')
+  setTimeout(() => {
+    searchWindow?.show()
+    searchWindow?.setCursorVisible(true)
+  }, 160) // 稍微延迟一点点可以确保渲染已完成
+})
